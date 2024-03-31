040af4e4aac02174b5451cb21c04a7e2cbf35fbb
Helinton Santos <helintonsantos@bemol.com.br>
2024-03-31 09:19:57 -0400

    Revert "Alterando pra 40"
    This reverts commit feb5c8b14fee8b738d74e30709fe84f58377cd01.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let listaDeNumerosSorteados = [];
 
-let numeroLimite = 40;
+let numeroLimite = 35;
 
 
 let numeroSecreto = gerarNumeroAleatorio();
@@ -15,7 +15,7 @@ function exibirTextoNaTela(tag, texto) {
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
 
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 40');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 35');
 
 }
 
